b83da7dcedb6315e54e7bae9bd387029c720704b
Krzyś <krzysworonczuk@gmail.com>
2024-04-11 13:33:57 +0200

    zadanie 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string name = "Ewa";
+var gender = "Female";
+var age = 25l;
+if (gender == "Female" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 30) 
+{
+    Console.WriteLine("Ewa,lat 30");
+}
+else if (gender == "male" && age < 18) 
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else
+{
+    Console.WriteLine("Nie spełnia żadnego warunku");
+}
+        
